3f247f00aeb6489ded4858a4b79fdcc00b6a7a38
Ankit Sinha <ankits.brainerhub@gmail.com>
2026-09-12 16:59:23 +0530

    feat(portfolio): integrate ambient universe and adaptive quality

--- src/components/ProjectScene.tsx
+++ src/components/ProjectScene.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useMemo, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
+import AmbientUniverse from './AmbientUniverse';
+import ScenePerformance from './ScenePerformance';
 
 const PROJECTS = ['aurora', 'echo', 'ledger', 'drift', 'halo'];
 type SceneState = { active: number; progress: number; visible: boolean };
@@ -48,5 +50,5 @@ export default function ProjectScene() {
       child.userData.depth = workProgress;
     });
   });
-  return <group ref={groups}>{PROJECTS.map((project) => <group key={project}><Artifact index={PROJECTS.indexOf(project)} intensity={1} /></group>)}</group>;
+  return <group><ScenePerformance /><AmbientUniverse /><group ref={groups}>{PROJECTS.map((project) => <group key={project}><Artifact index={PROJECTS.indexOf(project)} intensity={1} /></group>)}</group></group>;
 }
