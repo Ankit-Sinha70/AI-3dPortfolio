51613fea3ec9195ed9623d213bb032989db6e37b
Ankit Sinha <ankits.brainerhub@gmail.com>
2026-09-12 16:58:16 +0530

    perf(portfolio): integrate adaptive scene quality

--- src/components/ProjectScene.tsx
+++ src/components/ProjectScene.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import AmbientUniverse from './AmbientUniverse';
+import ScenePerformance from './ScenePerformance';
 
 const PROJECTS = ['aurora', 'echo', 'ledger', 'drift', 'halo'];
 type SceneState = { active: number; progress: number; visible: boolean };
@@ -49,5 +50,5 @@ export default function ProjectScene() {
       child.userData.depth = workProgress;
     });
   });
-  return <group><AmbientUniverse /><group ref={groups}>{PROJECTS.map((project) => <group key={project}><Artifact index={PROJECTS.indexOf(project)} intensity={1} /></group>)}</group></group>;
+  return <group><ScenePerformance /><AmbientUniverse /><group ref={groups}>{PROJECTS.map((project) => <group key={project}><Artifact index={PROJECTS.indexOf(project)} intensity={1} /></group>)}</group></group>;
 }
